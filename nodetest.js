95adb724fadf75a5b579d36c0a10410e9ef03173
borrik <borrik@live.ru>
2018-08-01 10:47:27 +0300

    nodeJS example added

---
+++ nodetest.js
@@ -0,0 +1,74 @@
+const {readdirSync} = require("fs");
+const {createServer} = require("http");
+
+
+let myserver= createServer((request,response) => {
+
+    //root drive
+
+    if (request.url == '/') {
+        let rootdir = readdirSync("/");
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(`
+    <h1>NodeJS</h1>
+    
+    <p>${rootdir.join("<br>")}</p>`);
+
+        response.end();
+    }
+
+
+    //pull    http://www.aquarium.ru/
+    else if(request.url == '/aquarium'){
+
+        const fetch = require('node-fetch');
+        fetch('http://aquarium.ru')
+            .then(res => res.text()).then((body) => {
+
+
+        response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(`
+    <h1>NodeJS</h1>
+    
+    <p>${body}</p>`);
+            response.end()
+        });
+    }
+    //request to eloquent
+    else if(request.url == '/eloquent') {
+        const {request} = require("http");
+        let requestStream =
+            request({
+                hostname: "eloquentjavascript.net",
+                path: "/20_node.html",
+                method: "GET",
+                headers: {Accept: "text/html"}
+            }, resp => {
+                console.log("Server responded with status code",
+                    resp.statusCode);
+                response.writeHead(200, {"Content-Type": "text/html"});
+                response.write(`
+    <h1>NodeJS</h1>
+    
+    <p>${JSON.stringify(resp.headers)}</p>`);
+                response.end()
+
+            });
+        requestStream.end();
+    }
+//ivalid request
+    else {
+        response.writeHead(404, {"Content-Type": "text/html"});
+        response.write(`
+    <p style="font-size: 50px; font-family: 'Franklin Gothic Medium'">
+    404 <br> </p>
+    Resourse not availalable <br> 
+    <code>${request.url}</code>`);
+
+        response.end();
+
+    }
+})
+
+myserver.listen(8000);
+console.log("Listening! (port 8000)");
