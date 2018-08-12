fa40fbef0e0f4a1f8b352bf801c0b21b4211d624
Boris <borrik@live.ru>
2018-08-12 21:51:22 +0300

    nodejs test

--- searcht.js
+++ searcht.js
@@ -1,17 +1,36 @@
 
-const {sep} = require("path");
-const {basename}=require("path");
-const {readFile} = require("fs");
+const {basename,sep} = require("path");
+
+const {readFile, stat} = require("fs");
+
 let searchxt =  new RegExp(process.argv[2]);
 let filelist=[];
 process.argv.slice(3).forEach(file=>{filelist.push(process.cwd()+sep+file)});
-console.log(filelist);
 
-filelist.forEach(async f=>{
-    await readFile(f, "utf8", (error, text) => {
-        if (error) throw error;
-        console.log(`The file ${basename(f)} ${searchxt.test(text)? 'contains': 'not contains'} ${process.argv[2]}`);
-    })
 
-})
-//processFiles(filelist)
+ function searchrun(filelist) {
+console.log('filelist',filelist);
+filelist.forEach(async f=> {
+
+    await  stat(f, async (err, st) => {
+        //console.log(st);
+        if (st.isDirectory()) {
+            console.log(f, 'is directory,getting content')
+            searchrun([f]);
+        }
+        else {
+            await readFile(f, "utf8", async (error, text) => {
+
+                if (error) throw error;
+
+                else {
+
+                    console.log(`The file ${basename(f)} ${searchxt.test(text) ? 'contains' : 'not contains'} ${process.argv[2]}`);
+                }
+            })
+        }
+
+    });
+})};
+
+searchrun(filelist)
