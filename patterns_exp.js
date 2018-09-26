8be7bc7cc6e2b273cf9770e332c8ed3c443861b7
borrik <borrik@live.ru>
2018-09-26 16:30:52 +0300

    new exrsices

--- patterns_exp.js
+++ patterns_exp.js
@@ -1,39 +1,287 @@
-function ObserverList() {
-    this.observerList = [];
-}
+//
+// 338
+// down vote
+// Very simply said, new X is Object.create(X.prototype) with additionally running the constructor function. (And giving the constructor the chance to return the actual object that should be the result of the expression instead of this.)
+//
+// That’s it. :)
+//
+// The rest of the answers are just confusing, because apparently nobody else reads the definition of new either. ;)
 
-ObserverList.prototype.add = function (obj) {
-    return this.observerList.push(obj);
+function Sclass() {
+    this.cll='cll'
 };
 
-ObserverList.prototype.count = function () {
-    return this.observerList.length;
-};
+Sclass.prototype.clname='sclass';
 
-ObserverList.prototype.get = function (index) {
-    if (index > -1 && index < this.observerList.length) {
-        return this.observerList[index];
-    }
-};
+var tt= new Sclass();
+//console.log(tt.clname);
 
-ObserverList.prototype.indexOf = function (obj, startIndex) {
-    var i = startIndex;
+///
 
-    while (i < this.observerList.length) {
-        if (this.observerList[i] === obj) {
-            return i;
-        }
-        i++;
-    }
+function Gclass() {};
 
-    return -1;
-};
+//subclassing - via copyng trough Object.create// Object.create(Classobj.prototype) or  new Classobj()
+Gclass.prototype=Object.create(Sclass.prototype);
 
-ObserverList.prototype.removeAt = function (index) {
-    this.observerList.splice(index, 1);
-};
+Gclass.prototype.newp='newpp'
 
 
+var gc = new Gclass();
+console.log(gc.clname);
+console.log(gc.newp);
+
+
+
+
+// with object
+// var newMainSh= {
+//     getsh:'shhh'
+// }
+//
+// newMainSh.gotsh = 'gotsh'
+//
+// function Newconstr (){};
+//
+// Newconstr.prototype=Object.create(newMainSh)
+//
+// Newconstr.prototype.gitsh='gitch'
+//
+// var newf = Object.create(Newconstr.prototype)
+//
+// console.log(newf.gitsh)
+// console.log(newf.getsh)
+//
+// console.log(newf.gotsh)
+
+//console.log(newMainSh.gitsh)
+
+
+
+//console.log(gitch)
+//
+//
+// var to = new Gclass();
+//
+// console.log(to.clname)
+
+// function obo1 () {
+// //constructor
+//     if (this.kio){
+//         this.kio = 'kio'
+//     }
+//     this.weba='weeba'
+//     //console.log(this.kio)
+// }
+// this.kio='m'
+// obo1.call(this)
+// //by calling  obo1 there, kio was pulled to 'this' [reality]
+// console.log(this.kio)
+// console.log(this.weba)
+//
+// //calling  to drive properties from superclass
+// //superclass
+// function Product(name, price) {
+//     this.name = name;
+//     this.price = price;
+//     this.intnl = 'intrnl'
+// }
+// //derived class
+// function Food(namo, prico) {
+//     Product.call(this, namo, prico);
+//     console.log(this.intnl)
+//     this.category = 'food';
+// }
+//
+//
+//
+// console.log(new Food('cheese', 5).name);
+// expected output: "cheese"
+
+// function Glass () {
+//
+// }
+// Glass.prototype=Sclass;
+//
+// //console.log(glass.clname)
+// Glass.prototype.tato='y'
+//
+// let glas= new Glass();
+//
+// console.log(glas.clname)
+
+// function Car(){};
+// Car.prototype.drive= 'drive';
+// Car.prototype.breakDown = 'break';
+//
+// function Truck(){};
+// var abstractVehicleFactory = (function () {
+//     // Storage for our vehicle types
+//     var types = {};
+//     return {
+//         getVehicle: function ( type, customizations ) {
+//             //console.log(types)
+//             var Vehicle = types[type];
+//             return (Vehicle ? new Vehicle(customizations) : null);
+//         },
+//         registerVehicle: function ( type, Vehicle ) {
+//             var proto = Vehicle.prototype;
+//             //console.log(proto.drive)
+//             // only register classes that fulfill the vehicle contract
+//             if ( proto.drive && proto.breakDown ) {
+//                 types[type] = Vehicle;
+//                 //console.log('registered',types)
+//             }
+//             return abstractVehicleFactory;
+//         }
+//     };
+// })();
+//
+// // Usage:
+//
+// abstractVehicleFactory.registerVehicle( "carr", Car );
+// console.log(abstractVehicleFactory)
+//var abstruck = abstractVehicleFactory.registerVehicle( "truck", Truck );
+
+//console.log(abstractVehicleFactory.getVehicle('carr'))
+
+//console.log(abstruck)
+
+// Instantiate a new car based on the abstract vehicle type
+// var car = abstractVehicleFactory.getVehicle( "car", {
+//     color: "lime green",
+//     state: "like new" } );
+//
+// // Instantiate a new truck in a similar manner
+// var truck = abstractVehicleFactory.getVehicle( "truck", {
+//     wheelSize: "medium",
+//     color: "neon yellow" } );
+
+// const jQuery= require('jquery')
+//
+// var pubsub = {};
+//
+// (function(myObject) {
+//
+//     // Storage for topics that can be broadcast
+//     // or listened to
+//     var topics = {};
+//
+//     // A topic identifier
+//     var subUid = -1;
+//
+//     // Publish or broadcast events of interest
+//     // with a specific topic name and arguments
+//     // such as the data to pass along
+//     myObject.publish = function( topic, args ) {
+//
+//         if ( !topics[topic] ) {
+//             return false;
+//         }
+//
+//         var subscribers = topics[topic],
+//             len = subscribers ? subscribers.length : 0;
+//
+//         while (len--) {
+//             subscribers[len].func( topic, args );
+//         }
+//
+//         return this;
+//     };
+//
+//     // Subscribe to events of interest
+//     // with a specific topic name and a
+//     // callback function, to be executed
+//     // when the topic/event is observed
+//     myObject.subscribe = function( topic, func ) {
+//
+//         if (!topics[topic]) {
+//             topics[topic] = [];
+//         }
+//
+//         var token = ( ++subUid ).toString();
+//         topics[topic].push({
+//             token: token,
+//             func: func
+//         });
+//         return token;
+//     };
+//
+//     // Unsubscribe from a specific
+//     // topic, based on a tokenized reference
+//     // to the subscription
+//     myObject.unsubscribe = function( token ) {
+//         for ( var m in topics ) {
+//             if ( topics[m] ) {
+//                 for ( var i = 0, j = topics[m].length; i < j; i++ ) {
+//                     if ( topics[m][i].token === token ) {
+//                         topics[m].splice( i, 1 );
+//                         return token;
+//                     }
+//                 }
+//             }
+//         }
+//         return this;
+//     };
+// }( pubsub ));
+//
+//
+//
+//
+// let extend =function (obj, expantion) {
+//     for(ex of Object.keys(expantion)){
+//        //console.log(ex)
+//         obj[ex]=expantion[ex]
+//     }
+//
+// };
+//
+// extend(jQuery,pubsub);
+//
+//
+// (function($){
+//     $.publish( "/new/user", { name: 'rf' } );
+//     $("#add").on("click", function( e ) {
+//         e.preventDefault()
+//     });
+// }(jQuery));
+//
+
+// function ObserverList() {
+//     this.observerList = [];
+// }
+//
+// ObserverList.prototype.add = function (obj) {
+//     return this.observerList.push(obj);
+// };
+//
+// ObserverList.prototype.count = function () {
+//     return this.observerList.length;
+// };
+//
+// ObserverList.prototype.get = function (index) {
+//     if (index > -1 && index < this.observerList.length) {
+//         return this.observerList[index];
+//     }
+// };
+//
+// ObserverList.prototype.indexOf = function (obj, startIndex) {
+//     var i = startIndex;
+//
+//     while (i < this.observerList.length) {
+//         if (this.observerList[i] === obj) {
+//             return i;
+//         }
+//         i++;
+//     }
+//
+//     return -1;
+// };
+//
+// ObserverList.prototype.removeAt = function (index) {
+//     this.observerList.splice(index, 1);
+// };
+//
+//
 
 // class Observer {
 //     constructor(params) {
